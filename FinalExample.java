9cbd3c2c986a7ccc1d96ec1bd491b75eeaa36bb7
kim jiyoon <gkswls108903@naver.com>
2019-07-15 18:07:25 +0900

    java pratice

---
+++ FinalExample.java
@@ -0,0 +1,17 @@
+
+public class FinalExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final double pi=3.14;
+		double radius=2.0;
+		
+		double cir=radius * pi * 2; // 원의 둘레
+		System.out.println("원의 둘레는"+cir);
+		
+		double area=pi*radius*radius;
+		System.out.println("원의 넓이는"+area);
+	}
+
+}
