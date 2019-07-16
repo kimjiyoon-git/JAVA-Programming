7756f6adefbfc6d16effdf43dcd4c23bd4206984
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:09:08 +0900

    Test6

---
+++ TEST6.java
@@ -0,0 +1,16 @@
+
+public class TEST6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int x = 4, y,value;
+	
+	Integer a=4;
+	
+	System.out.println(x);
+	System.out.println(a.toBinaryString(x)); //2Áø¼ö 4=(100)
+	
+	}
+
+}
