565052c1856ec4412aac7f219700c8153659addf
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:25:36 +0900

    Test23.java

---
+++ Test23.java
@@ -0,0 +1,14 @@
+
+public class Test23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mclass[]= {"비트","자바","수업"};
+for(String s : mclass)
+{
+	System.out.println(s);
+	}
+
+	}
+
+	}
