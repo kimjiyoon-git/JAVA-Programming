2bae1ae0b20fd9d2e22a7425ba48b68f950753bd
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:12:22 +0900

    Test8

---
+++ Test8.java
@@ -0,0 +1,20 @@
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="HELLO", b="hello",c="Hello",d="Kim ji yoon",e="KIM JI YOON";
+		
+		System.out.println(a.substring(0,3));
+		System.out.println(a.substring(0,5));
+		System.out.println(a.substring(0,4));
+		System.out.println(a.substring(0,1));
+		
+		
+		System.out.println(c.toUpperCase());
+		System.out.println(a.toLowerCase());
+		System.out.println(d.toUpperCase());
+		System.out.println(e.toLowerCase());
+	}
+
+}
