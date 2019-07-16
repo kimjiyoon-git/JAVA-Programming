222660f5d18c39916907f5450b5ac2b48eeef2cf
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:27:13 +0900

    Test34.java

---
+++ Test34.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Test34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> mclass=new ArrayList<>();
+		mclass.add(2015);
+		mclass.add(11);
+		mclass.add(18);
+	
+		for(Integer i : mclass)
+		{
+			System.out.println(i);
+			}
+
+			}
+
+			}
