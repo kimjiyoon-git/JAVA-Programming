147f998bd653a93399121c2a32a1365f6013c84c
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:06:55 +0900

    Test22

---
+++ Test22.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Test22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int fat=35;
+		
+		
+		    if(fat>=40) {
+			if(fat>=50)
+			System.out.println(" A등급입니다 ");
+			else 
+			System.out.println(" B등급입니다 ");
+		    }
+		   
+			else 
+			if(fat>=30)
+			System.out.println(" C등급입니다 ");
+			else
+			System.out.println(" D등급입니다 ");
+			
+		
+		}
+		
+
+	}
