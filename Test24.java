7d514e8884ff9ebeb9b388ecbbaafce5ffa75806
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:25:57 +0900

    Test24.java

---
+++ Test24.java
@@ -0,0 +1,15 @@
+
+public class Test24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String mclass[]= {"비트","자바","수업"};
+		for(int i=0;i<mclass.length;i++)
+		{
+			System.out.println(mclass[0]);
+	}
+
+}
+
+}
