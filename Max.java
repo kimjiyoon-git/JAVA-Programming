314f62af95e182adfaefff7977cebca8dabd5883
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:19:33 +0900

    Max.java

---
+++ Max.java
@@ -0,0 +1,20 @@
+import java.math.*;
+public class Max {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		int y=3;
+		
+		if(x>y) 
+			System.out.println("X°¡ ´õ Å®´Ï´Ù");
+			else
+			System.out.println("Y°¡ ´õ Å®´Ï´Ù");
+				
+		
+		}
+	}
+	
+
+
+
