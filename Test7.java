ab533c5394eb178857b6b40cfd839e89a2ed1995
kim jiyoon <gkswls108903@naver.com>
2019-07-16 16:11:05 +0900

    Test7

---
+++ Test7.java
@@ -0,0 +1,16 @@
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="Hello",b="hello",c="Hello";
+		
+		
+		System.out.println(a.equals(b));
+		System.out.println(a.replace("l","L"));
+		//System.out.println(c.replaceAll("l","L"));
+		System.out.println(c.replaceFirst("l","L"));
+		
+	}
+
+}
