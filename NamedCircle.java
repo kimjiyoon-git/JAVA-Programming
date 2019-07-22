9bbfe1d43730bd9f47669348069591e1a793c107
kimjiyoon-study <gkswls108903@naver.com>
2019-07-22 21:44:32 +0900

    NakedCircle.java

---
+++ NamedCircle.java
@@ -0,0 +1,30 @@
+class Circle {
+	
+	private int radius;
+	public Circle(int radius) {  this.radius= radius; }
+	public int getRadius() { return radius; }
+	
+}
+
+public class NamedCircle extends Circle {
+	private String name;
+	
+	
+
+	NamedCircle(int radius,String name) {
+		super(radius);
+		this.name= name;
+	
+	}
+	
+		public void show() {
+		System.out.println(name+", ¹ÝÁö¸§=  "+getRadius());	
+		}
+		
+	public static void main(String[] args) {
+	NamedCircle w= new NamedCircle(5,"chocolate");
+		w.show();
+
+	}
+
+}
