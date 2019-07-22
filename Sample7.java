87109633726c8956329b8ebef9822198d3dd2b21
kimjiyoon-study <gkswls108903@naver.com>
2019-07-22 22:28:57 +0900

    Sample7.java

---
+++ Sample7.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+abstract class Test1{
+abstract int add(int a, int b); 
+
+}
+
+public class Sample7 extends Test1 {
+public  int add(int a, int b) {
+		return a+b; // 메소드 
+	}
+		
+		
+	
+public static void main(String[] args) {
+	
+	Scanner sc= new Scanner(System.in);
+	Sample7 s= new Sample7();
+	int a;
+	int b;
+	int sum;
+	System.out.println("숫자를 입력하시오");
+	
+	a=sc.nextInt();
+	
+	System.out.println("숫자를 입력하시오");
+	b=sc.nextInt();
+	
+	//sum=a+b;
+	
+	System.out.println("두 수의 합은:" + s.add(a,b));
+
+	sc.close();
+	}
+
+}
+
