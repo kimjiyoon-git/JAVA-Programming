5a1781b8ecabd2f0ebe7a0015e22322266a9403a
kimjiyoon-study <gkswls108903@naver.com>
2019-07-23 18:10:39 +0900

    VideoShopsame.java

---
+++ VideoShopsame.java
@@ -0,0 +1,14 @@
+
+   /**
+   IQueue와 Shop을 동시에 상속하고 구현한 예
+   **/
+   public class VideoShop extends Shop implements IQueue{
+       public void enQueue(String video){
+           System.out.println(video + "반납");
+           this.store.addElement(video);    
+       }
+       public String deQueue(){
+           return (String)this.store.remove(0);
+       }
+   } //end of VideoShop class
+
