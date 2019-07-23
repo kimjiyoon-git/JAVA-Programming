21837c177faf7cfecf2be1175fd90ffd6dbe294e
kimjiyoon-study <gkswls108903@naver.com>
2019-07-23 18:22:52 +0900

    hello

---
+++ IRemoteControlsame2.java
@@ -0,0 +1,13 @@
+
+
+/**
+02:   IRemoteControl 인터페이스
+03:   **/
+ public interface IRemoteControl{
+      void powerOn(); //추상 메서드
+      void powerOff(); //추상 메서드
+       void channelUp(); //추상 메서드
+      void channelDown(); //추상 메서드
+	
+ }
+ 
