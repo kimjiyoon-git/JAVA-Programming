920fe6cd986df07eb0cdc520fa8a719457c6b59f
kimjiyoon-study <gkswls108903@naver.com>
2019-07-23 18:14:19 +0900

    java

---
+++ BodySign1interfacesame2.java
@@ -0,0 +1,12 @@
+public interface BodySign1 {
+       //1. 키워드를 명시하지 않은 경우
+       int CENTER = 1;
+       int LEFT = 2;
+       int RIGHT = 3;
+       int DOWN = 4;
+       int UP = 5;
+       void throwBall(int how);
+
+ 
+   } //end of BodySign interface
+
