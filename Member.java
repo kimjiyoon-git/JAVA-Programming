9e0924da677ead1883acbda2de4c47ddfa741329
kimjiyoon-study <gkswls108903@naver.com>
2019-07-23 17:49:36 +0900

    Member.java

---
+++ Member.java
@@ -0,0 +1,24 @@
+
+public class Member {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age;
+		String name;
+		String id;
+		String password;
+		
+		age=21;
+		name="김지윤";
+		id="gkswls108903";
+		password="055646064";
+		
+		System.out.println("나이:" + age);
+		System.out.println("이름:" + name);
+		System.out.println("아이디:" + id);
+		System.out.println("비밀번호:" + password);
+		
+	}
+
+}
