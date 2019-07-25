4d47b21abdbd49e031c3d9de08e6a8ea529e7802
kimjiyoon-study <gkswls108903@naver.com>
2019-07-25 19:42:53 +0900

    ToStringMain.java

---
+++ ToStringMain.java
@@ -0,0 +1,11 @@
+class Toy extends Object{
+    //내용없음
+} //end of Toy class
+
+    public class ToStringMain{
+        public static void main(String[] args){
+            Toy t = new Toy();
+            System.out.println(t);
+            System.out.println(t.toString());
+        } //end of main
+    } //end 
