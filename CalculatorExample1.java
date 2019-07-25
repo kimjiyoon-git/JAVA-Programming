d7c66e70b3e2cc3d1f8e08974f954994eba14b12
kimjiyoon-study <gkswls108903@naver.com>
2019-07-25 18:32:56 +0900

    Calculator.java set

---
+++ CalculatorExample1.java
@@ -0,0 +1,19 @@
+
+public class CalculatorExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double result1=10 * 10 * Calculator1.pi;
+		
+		int result2=Calculator1.plus(10,5);
+		int result3=Calculator1.minus(10,5);
+		
+		System.out.println("result1:"+result1);
+		System.out.println("result2:"+result2);
+		System.out.println("result3:"+result3);
+	}
+
+}
+
+// 인스턴스를 안 만들어주었다. 
