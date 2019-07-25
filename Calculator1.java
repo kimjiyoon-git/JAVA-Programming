3a8486103b6ecd6d2409104a3adbde33054f182d
kimjiyoon-study <gkswls108903@naver.com>
2019-07-25 18:32:28 +0900

    CalculatorExample1.java set

---
+++ Calculator1.java
@@ -0,0 +1,15 @@
+
+public class Calculator1 {
+
+	static double pi=3.14159;
+	
+	static int plus(int x, int y) { //static이 없다면 뒤에  Calculator cc= new Calculator();로 수정 후 밑에  9번째 줄  Calculator1를  cc로 변경
+		return x+y;
+	}
+	static int minus(int x, int y) {
+		return x-y;
+	}
+}
+
+
+//
