4dede25a8280100069a6a5008e8d6259c9c8c8be
kIMJIYOON <gkswls108903@naver.com>
2019-08-02 22:54:16 +0900

    Bit java test

---
+++ array_sum_for_each_test.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum=0;
+
+		
+		int d[] ={1, 3, 6, 7 ,9};
+		for(int i=0; i<d.length; i++) {
+			  sum += d[i];
+		}
+		for(int tmp : d) {
+	
+		}
+		System.out.println("배열 count 요소의 합은:"+d);
+	}
+	}
+
