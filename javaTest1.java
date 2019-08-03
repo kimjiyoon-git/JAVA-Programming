e1554c1c340db868006d484e6ef31a831547870d
kIMJIYOON <gkswls108903@naver.com>
2019-08-03 09:54:08 +0900

    190803_Bit java test

---
+++ javaTest1.java
@@ -0,0 +1,16 @@
+public class javaTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum= 0;
+		
+		int d[]={1, 3, 6, 7 ,9};
+		
+		
+		for(int i=0; i<d.length; i++)
+		  sum += d[i];
+		System.out.println(sum);
+	}
+
+}
